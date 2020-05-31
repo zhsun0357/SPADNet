85226aac3a66e213c63a9cba59f590c8521f7c4a
zhsun0357 <38968396+zhsun0357@users.noreply.github.com>
2020-05-31 00:03:21 -0700

    evaluation code

--- evaluate_spadnet.py
+++ evaluate_spadnet.py
@@ -17,6 +17,9 @@ import scipy
 import scipy.io
 import os
 import pathlib
+import re
+import json
+from matrices_spadnet import delta, rel_abs_diff, rel_sqr_diff
 
 import pdb
 
@@ -46,6 +49,8 @@ parser.add_argument('--spad_datapath', default=None, type=str, metavar='PATH',
                     help='path to SPAD measurement data')
 parser.add_argument('--mono_datapath', default=None, type=str, metavar='PATH',
                     help='path to monocular depth estimations')
+parser.add_argument('--matrices_out', default=None, type=str, metavar='PATH',
+                    help='path to output evaluated matrices')
 
 # log-scale rebinning parameters
 Linear_NUMBIN = 1024
@@ -127,76 +132,128 @@ def parse_arguments(args):
     opt['out_datapath'] = config.get(option, 'out_datapath')
     opt['spad_datapath'] = config.get(option, 'spad_datapath')
     opt['mono_datapath'] = config.get(option, 'mono_datapath')
-    
+    opt['matrices_out'] = config.get(option, 'matrices_out')
+
     return opt
 
-def get_output_file(datapath, filename):
 
-    out_filename = filename.replace('.mat', '.npy')
-    out_filename = out_filename.replace(datapath, '')
-    out_filename = out_filename.replace('/spad', '')
+class eval_module():
+    def __init__(self, opt, model, val_loader, model_name = 'SPADnet'):
 
-    return out_filename
+        self.opt = opt
+        self.val_loader = val_loader
+        self.model = model
+        self.model_name = model_name
+
+        self.loss_fns = []
+        self.loss_fns.append(("delta1", lambda p, t, m: delta(p, t, m, threshold=1.25)))
+        self.loss_fns.append(("delta2", lambda p, t, m: delta(p, t, m, threshold=1.25 ** 2)))
+        self.loss_fns.append(("delta3", lambda p, t, m: delta(p, t, m, threshold=1.25 ** 3)))
+        self.loss_fns.append(("rel_abs_diff", rel_abs_diff))
+        self.loss_fns.append(("rel_sqr_diff", rel_sqr_diff))
+        self.total_losses = {loss_name: 0 for loss_name, _ in self.loss_fns}
+        self.total_losses['rmse'] = 0
+
+        self.Num_pixels = 0
+        self.n_iters = 0
 
 
-def process_denoise(opt, model, val_loader, n_iter, model_name='SPADnet'):
-    
-    print('=> Evaluating Model on NYUV2 Dataset...\n')
+    def get_output_file(self, filename):
 
-    if not os.path.exists(opt['out_datapath']):
-        os.mkdir(opt['out_datapath'])
+        out_filename = filename.replace('.mat', '.npy')
+        out_filename = out_filename.replace(self.opt['spad_datapath'], '')
+        subfolder = re.search(r'\w+/spad_', out_filename).group(0)
+        subfolder = self.opt['out_datapath'] + subfolder.replace('spad_', '')
+        out_filename = out_filename.replace('spad_', '')
 
-    for sample in tqdm(val_loader):
+        return out_filename, subfolder
 
-        spad = sample['spad']
-        intensity = sample['intensity']
-        depth = sample['depth_hr']
-        mono_pred = sample['mono_pred']
-        filename = sample['filename']
+    def calculate_matrices(self, pred, depth, mask):
+        pred *= 12.276 ## convert to unit meter
+        depth *= 12.276 ## convert to unit meter
+        for loss_name, loss_fn in self.loss_fns:
+            loss = loss_fn(pred,depth,mask)
+            self.total_losses[loss_name] += loss * np.sum(mask)
+        self.total_losses['rmse'] += np.sum(((pred - depth) * mask)**2)
+        self.Num_pixels += np.sum(mask)
 
-        spad_var = Variable(spad.type(dtype))
-        depth_var = Variable(depth.type(dtype))
-        intensity_var = Variable(intensity.type(dtype))
-        mono_pred_var = Variable(mono_pred.type(dtype))
 
-        batchsize, _, H, W = depth_var.size()
-        mono_pred_var = mono_pred_var.view(batchsize, 1, H, W)
-        pdb.set_trace()
-        ## patch size. Use 128x128 patches, with overlapping (64 step size)
-        dim1 = 128
-        dim2 = 128
-        step = 64
-        num_row = int(np.floor(H /step))
-        num_col = int(np.floor(W /step))
+    def summary_matrices(self):
+        print('=> Summarizing Matrices\n')
+        self.avg_losses = {loss_name: self.total_losses[loss_name]/self.Num_pixels for loss_name in self.total_losses}
+        self.avg_losses['rmse'] = np.sqrt(self.total_losses['rmse'] / self.Num_pixels)
 
-        sargmax = torch.zeros(batchsize, H, W).cuda()
+        with open(self.opt['matrices_out'], 'w') as f:
+            json.dump(self.avg_losses, f)
 
-        for ii in range(num_row):
-            for jj in range(num_col):
 
-                spad_patch = spad_var[:, :, :, ii*step:(ii*step + dim1), jj*step:(jj*step + dim2)]
-                mono_pred_patch = mono_pred_var[:, :, ii*step:(ii*step + dim1), jj*step:(jj*step + dim2)]
-                spad_patch = tologscale(spad_patch, NUMBIN, Q)
-                mono_rates_patch = dmap2pc(mono_pred_patch, NUMBIN, Q, Linear_NUMBIN)
-                mono_rates_patch = Variable(mono_rates_patch.type(dtype))
+    def process_denoise(self):
+
+        print('=> Evaluating Model on NYUV2 Dataset...\n')
+
+        if not os.path.exists(self.opt['out_datapath']):
+            os.mkdir(self.opt['out_datapath'])
+
+        for sample in tqdm(self.val_loader):
+
+            spad = sample['spad']
+            intensity = sample['intensity']
+            depth = sample['depth_hr']
+            mono_pred = sample['mono_pred']
+            mask = sample['mask']
+            filename = sample['filename']
+
+            spad_var = Variable(spad.type(dtype))
+            depth_var = Variable(depth.type(dtype))
+            intensity_var = Variable(intensity.type(dtype))
+            mono_pred_var = Variable(mono_pred.type(dtype))
+
+            batchsize, _, H, W = depth_var.size()
+            mono_pred_var = mono_pred_var.view(batchsize, 1, H, W)
+            ## patch size. Use 128x128 patches, with overlapping (64 step size)
+            dim1 = 128
+            dim2 = 128
+            step = 64
+            num_row = int(np.floor(H /step))
+            num_col = int(np.floor(W /step))
+
+            sargmax = torch.zeros(batchsize, H, W).cuda()
+
+            for ii in range(num_row):
+                for jj in range(num_col):
+
+                    spad_patch = spad_var[:, :, :, ii*step:(ii*step + dim1), jj*step:(jj*step + dim2)]
+                    mono_pred_patch = mono_pred_var[:, :, ii*step:(ii*step + dim1), jj*step:(jj*step + dim2)]
+                    spad_patch = tologscale(spad_patch, NUMBIN, Q)
+                    mono_rates_patch = dmap2pc(mono_pred_patch, NUMBIN, Q, Linear_NUMBIN)
+                    mono_rates_patch = Variable(mono_rates_patch.type(dtype))
+                    
+                    denoise_out, sargmax_patch = self.model(spad_patch, mono_rates_patch)
+                    
+                    beginx = 0 if (ii == 0) else step//2
+                    endx = dim1 if (ii == (num_row - 1)) else (dim1 - step//2)
+                    beginy = 0 if (jj == 0) else step//2
+                    endy = dim2 if (jj == (num_col - 1)) else (dim2 - step//2)
+
+                    sargmax[:, (ii*step + beginx):(ii*step + endx), (jj*step + beginy):(jj*step + endy)] = sargmax_patch[:, 0, beginx:endx, beginy:endy]
+
+            for kk in range(batchsize):
+
+                out_filename, out_subfolder = self.get_output_file(filename[kk])
+                if not os.path.exists(out_subfolder):
+                    os.mkdir(out_subfolder)
+                out_filename = self.opt['out_datapath'] + out_filename
+                eval_pred = sargmax[kk,17: H - 15, 17: W - 15].squeeze().data.cpu().numpy()
+                eval_depth = depth[kk, 0, 17: H - 15, 17: W - 15].squeeze().cpu().numpy()
+                eval_mask = mask[kk, 17: H - 15, 17: W - 15].squeeze().cpu().numpy()
                 
-                denoise_out, sargmax_patch = model(spad_patch, mono_rates_patch)
-                
-                beginx = 0 if (ii == 0) else step//2
-                endx = dim1 if (ii == (num_row - 1)) else (dim1 - step//2)
-                beginy = 0 if (jj == 0) else step//2
-                endy = dim2 if (jj == (num_col - 1)) else (dim2 - step//2)
+                np.save(out_filename, eval_pred)
+                self.calculate_matrices(eval_pred, eval_depth, eval_mask)
+                # crop out the boundary region
+            self.n_iters += 1
 
-                sargmax[:, (ii*step + beginx):(ii*step + endx), (jj*step + beginy):(jj*step + endy)] = sargmax_patch[:, 0, beginx:endx, beginy:endy]
-
-        for kk in range(batchsize):
-            out_filename = get_output_file(opt['spad_datapath'], filename[kk])
-            out_filename = opt['out_datapath'] + out_filename
-            np.save(out_filename, sargmax[kk,17: H - 15, 17: W - 15].data.cpu().numpy())
-            # crop out the boundary region
-        n_iter += 1
-
-    print('=> Evaluating Model on NYUV2 Dataset Finished\n')
+        self.summary_matrices()
+        print('=> Evaluating Model on NYUV2 Dataset Finished\n')
 
 
 def main():
@@ -215,7 +272,6 @@ def main():
     for p in model.parameters():
         p.requires_grad = False
 
-    pdb.set_trace()
     print('=> Loading checkpoint {}'.format(opt['checkpoint'][0]))
     ckpt = torch.load(opt['checkpoint'][0])
     model_dict = model.state_dict()
@@ -230,7 +286,6 @@ def main():
     
     model.load_state_dict(model_dict)
 
-    pdb.set_trace()
     ################# Dataset loading #################
     val_dataset = \
     SpadDataset(opt['test_files'], opt['ckpt_noise_param_idx'],
@@ -242,9 +297,8 @@ def main():
                             pin_memory=True)
 
     ################# Start evaluate #################
-    n_iter = 0
-    process_denoise(opt, model, val_loader, n_iter, model_name=opt['model_name'])
-
+    spadnet_eval = eval_module(opt, model, val_loader, model_name = opt['model_name'])
+    spadnet_eval.process_denoise()
 
 if __name__ == '__main__':
     main()
